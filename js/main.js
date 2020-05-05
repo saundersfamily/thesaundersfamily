d6b9a478fe41e3cfa5592e7937c77eb4e8cf2daf
Anthony Saunders <anthony.k.saunders@gmail.com>
2020-05-05 16:11:44 -0400

    Initial Commit
    First commit for the Saunders Family Website

---
+++ js/main.js
@@ -0,0 +1,63 @@
+// Smooth Scroll Script
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  // End of Smooth Scroll Script
+
+// Scroll to Top Script
+$(document).ready(function () {
+
+var elem   = $('#story1');  
+    
+var bottom = $(elem).position().top+$(elem).outerHeight(true)
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > bottom) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+
+    $('#scroll').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+        return false;
+    });
+
+});
+
+// End of Scroll to Top Script
